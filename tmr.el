11c3da3559e7e1cc58b642d438706d66ab9f8a5e
Protesilaos Stavrou <info@protesilaos.com>
2022-04-21 07:02:59 +0300

    Add FSF as copyright holder

--- tmr.el
+++ tmr.el
@@ -1,6 +1,6 @@
 ;;; tmr.el --- TMR Must Recur -*- lexical-binding: t -*-
 
-;; Copyright (C) 2020-2022  Protesilaos Stavrou
+;; Copyright (C) 2020-2022  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://protesilaos.com/dotemacs
