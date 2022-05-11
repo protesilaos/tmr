protesilaos/tmr

commit 2fc61f95950b564cb3ceefe57f1fec339adc2969

WIP: Create tabulated-list view + use a struct for tmr--timers

tmr.el
@@ -115,6 +115,52 @@ It should take two string arguments: the title and the message."
   :type 'function
   :group 'tmr)
 
+(cl-defstruct (tmr-timer
+               (:constructor tmr--timer-create)
+               (:conc-name tmr--timer-))
+  (creation-date
+   nil
+   :read-only t
+   :documentation "Time at which the timer was created.")
+  (duration
+   nil
+   :read-only t
+   :documentation "Number of seconds after `start' indicating when the timer finishes.")
+  (donep
+   nil
+   :read-only nil
+   :documentation "Non-nil if the timer is finished.")
+  (timer-object
+   nil
+   :read-only nil
+   :documentation "The object returned by `run-with-timer'.")
+  (description
+   nil
+   :read-only t
+   :documentation "Optional string describing the purpose of the timer, e.g., \"Stop the oven\"."))
+
+(defun tmr--long-description (timer)
+  "Return a human-readable description for TIMER."
+  (let ((start (tmr--format-creation-date timer))
+        (duration (tmr--timer-duration timer))
+        (description (tmr--timer-description timer)))
+    (if description
+        (format "Started at %s with input '%s' and description '%s'" start duration description)
+      (format "Started at %s with input '%s'" start duration))))
+
+(defun tmr--format-creation-date (timer)
+  "Return a string representing when TIMER was created."
+  (tmr--format-time (tmr--timer-creation-date timer)))
+
+(defun tmr--format-end-date (timer)
+  "Return a string representing when TIMER should finish."
+  (format-time-string "%T" (time-add (tmr--timer-creation-date timer)
+                                     (tmr--timer-duration timer))))
+
+(defun tmr--format-time (time)
+  "Return a human-readable string representing TIME."
+  (format-time-string "%T" time))
+
 (defun tmr--unit (time)
   "Determine common time unit for TIME."
   (cond
@@ -185,19 +231,21 @@ Read: (info \"(elisp) Desktop Notifications\") for details."
   "Send notification with TITLE and MESSAGE using `tmr-notify-function'."
   (funcall tmr-notify-function title message))
 
-(defun tmr--notify (start &optional description)
-  "Send notification for timer with START time.
-Optionally include DESCRIPTION."
-  (let ((end (format-time-string "%T"))
-        (desc-plain "")
-        (desc-propertized ""))
-    (when description
-      (setq desc-plain (concat "\n" description)
-            desc-propertized (concat " [" (propertize description 'face 'bold) "]")))
+(defun tmr--notify (timer)
+  "Send notification for TIMER."
+  (let* ((description (tmr--timer-description timer))
+         (desc-plain (if description
+                         (concat "\n" description)
+                       ""))
+         (desc-propertized (if description
+                               (concat " [" (propertize description 'face 'bold) "]")
+                             ""))
+         (start (tmr--format-creation-date timer))
+         (end (tmr--format-end-date timer)))
+    (setf (tmr--timer-donep timer) t)
     (tmr--notify-send-notification
      "TMR May Ring (Emacs tmr package)"
-     (format "Time is up!\nStarted: %s\nEnded: %s%s"
-             start end desc-plain))
+     (format "Time is up!\nStarted: %s\nEnded: %s%s" start end desc-plain))
     (message
      "TMR %s %s ; %s %s%s"
      (propertize "Start:" 'face 'success) start
@@ -239,7 +287,7 @@ multiple timers, prompt for one with completion."
 Optionally include DESCRIPTION."
   (let* ((specifier (substring time -1))
          (amount (substring time 0 -1))
-         (start (format-time-string "%T"))
+         (start (tmr--format-time (current-time)))
          (unit (pcase specifier
                  ("s" (format "%ss (s == second)" amount))
                  ("h" (format "%sh (h == hour)" amount))
@@ -302,19 +350,19 @@ command `tmr-with-description' instead of this one."
    (list
     (tmr--read-duration)
     (when current-prefix-arg (tmr--description-prompt))))
-  (let* ((start (format-time-string "%T"))
-         (unit (tmr--unit time))
-         (object-desc (if description
-                          (format "Started at %s with input '%s' and description '%s'" start time description)
-                        (format "Started at %s with input '%s'" start time))))
+  (let* ((creation-date (current-time))
+         (duration (tmr--unit time))
+         (timer (tmr--timer-create
+                 :description description
+                 :creation-date creation-date
+                 :duration duration))
+         (timer-object (run-with-timer
+                        duration nil
+                        #'tmr--notify timer)))
+    (setf (tmr--timer-timer-object timer) timer-object)
     (tmr--echo-area time description)
-    (push (cons
-           object-desc
-           (run-with-timer
-            unit nil
-            'tmr--notify start description))
-          tmr--timers)
-    (tmr--log-in-buffer object-desc)))
+    (push timer tmr--timers)
+    (tmr--log-in-buffer (tmr--long-description timer))))
 
 ;;;###autoload
 (defun tmr-with-description (time description)

tmr-tabulated.el
@@ -1,0 +1,69 @@
+;;; tmr-tabulated.el --- Display timers in a tabulated list -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020-2022  Free Software Foundation, Inc.
+
+;; Author: Damien Cassou <damien@cassou.me>,
+;;         Protesilaos Stavrou <info@protesilaos.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; TODO
+
+;;; Code:
+
+(require 'tmr)
+
+;;;###autoload
+(defun tmr-tabulated-view ()
+  "Open a tabulated list buffer listing tmr timers."
+  (interactive)
+  (switch-to-buffer (get-buffer-create "*tmr-tabulated-view*"))
+  (tmr-tabulated--set-entries)
+  (tmr-tabulated-mode)
+  (tabulated-list-print))
+
+(defun tmr-tabulated--set-entries ()
+  "Set the value of `tabulated-list-entries' with timers."
+  (setq-local tabulated-list-entries
+              (mapcar #'tmr-tabulated--timer-to-entry tmr--timers)))
+
+(defun tmr-tabulated--timer-to-entry (timer)
+  "Convert TIMER into an entry suitable for `tabulated-list-entries'."
+  (list (tmr--timer-creation-date timer)
+        (vector (tmr--format-creation-date timer)
+                (tmr--format-end-date timer)
+                (if (tmr--timer-donep timer) "✔" "")
+                (or (tmr--timer-description timer) ""))))
+
+(defvar tmr-tabulated-mode-map
+  (let ((map (make-sparse-keymap)))
+    map)
+  "Keybindings for `tmr-tabulated-mode-map'.")
+
+(define-derived-mode tmr-tabulated-mode tabulated-list-mode "TMR"
+  "Major mode to display tmr timers."
+  (setq-local tabulated-list-format
+              [("Start" 10 t)
+               ("End" 10 t)
+               ("Finished?" 10 t)
+               ("Description" 0 t)])
+  (add-hook 'tabulated-list-revert-hook #'tmr-tabulated--set-entries)
+  (tabulated-list-init-header))
+
+(provide 'tmr-tabulated)
+;;; tmr-tabulated.el ends here

commit 4d8c70530ce87a3bf76d2d4e383628abdc6d8e99

tmr-cancel: Fix
The code was broken after commit 2fc61f9 which was merged by mistake:

2fc61f95950b564cb3ceefe57f1fec339adc2969
Author:       Damien Cassou <damien@cassou.me>
AuthorDate:   Sat May 7 22:39:20 2022 +0200
Commit:       Protesilaos Stavrou <info@protesilaos.com>
CommitDate:   Sun May 8 06:48:45 2022 +0300

WIP: Create tabulated-list view + use a struct for tmr--timers

tmr.el
@@ -259,29 +259,34 @@ Read: (info \"(elisp) Desktop Notifications\") for details."
   "List of timer objects.
 Populated by `tmr' and then operated on by `tmr-cancel'.")
 
-;; FIXME 2022-05-08: Need to be refactored per commit 2fc61f9.
+(defun tmr--get-timer-by-creation-date (creation-date)
+  "Return the timer which was started at CREATION-DATE."
+  (cl-find creation-date tmr--timers :key #'tmr--timer-creation-date))
+
 ;;;###autoload
-(defun tmr-cancel ()
-  "Cancel timer object set with `tmr' command.
-If there is a single timer, cancel it outright.  If there are
-multiple timers, prompt for one with completion."
-  (interactive)
-  (if-let ((timers tmr--timers))
-      (cond
-       ((= (length timers) 1)
-        (let ((cell (car timers)))
-          (cancel-timer (cdr cell))
-          (tmr--log-in-buffer (format "CANCELLED <<%s>>" (car cell)))
-          (setq tmr--timers nil)))
-       ((> (length timers) 1)
-        (let* ((selection (completing-read "Cancel timer: " (mapc #'car timers) nil t))
-               (cell (assoc selection timers #'string-match-p))
-               (key (car cell))
-               (object (cdr cell)))
-          (cancel-timer object)
-          (tmr--log-in-buffer (format "CANCELLED <<%s>>" key))
-          (setq tmr--timers (delete cell tmr--timers)))))
-    (user-error "No `tmr' to cancel")))
+(defun tmr-cancel (timer)
+  "Cancel TIMER object set with `tmr' command.
+Interactively, let the user choose which timer to cancel with
+completion."
+  (interactive (list (tmr--read-timer)))
+  (if (not timer)
+      (user-error "No `tmr' to cancel")
+    (cancel-timer (tmr--timer-timer-object timer))
+    (tmr--log-in-buffer (format "CANCELLED <<%s>>" (tmr--long-description timer)))
+    (setq tmr--timers (cl-delete timer tmr--timers))))
+
+(defun tmr--read-timer ()
+  "Let the user choose a timer among all timers. Return the selected timer.
+If there is a single timer, use that.  If there are multiple
+timers, prompt for one with completion. If there are no timers,
+return nil."
+  (cond
+   ((= (length tmr--timers) 1)
+    (car timers))
+   ((> (length tmr--timers) 1)
+    (let* ((timer-descriptions (mapcar #'tmr--long-description tmr--timers))
+           (selection (completing-read "Timer: " timer-descriptions nil t)))
+      (cl-find selection tmr--timers :test #'string= :key #'tmr--long-description)))))
 
 (defun tmr--echo-area (time &optional description)
   "Produce `message' for current `tmr' TIME.